b0628ebbf170a5a44008211c6a646da7d5bd9dbb
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2025-08-24 23:08:46 +0900

    first commit

---
+++ lib/jsonc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jsonc/version"
+
+module Jsonc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jsonc/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Jsonc
+  VERSION = "0.1.0"
+end
