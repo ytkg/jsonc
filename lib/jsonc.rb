eccc0089daf836a5084410417e64a2f6492104b5
Tim Gentry <52189+timgentry@users.noreply.github.com>
2026-01-05 18:17:09 +0000

    Added max_bytes limit with 10MB default

--- lib/jsonc.rb
+++ lib/jsonc.rb
@@ -7,7 +7,15 @@ require_relative "jsonc/parser"
 module JSONC
   class Error < StandardError; end
 
+  DEFAULT_MAX_BYTES = 10_485_760 # 10MB
+
   def self.parse(string, **opts)
+    max_bytes = opts.delete(:max_bytes) || DEFAULT_MAX_BYTES
+    if string.bytesize > max_bytes
+      raise JSON::ParserError,
+            "input string too large (#{string.bytesize} bytes, max #{max_bytes} bytes)"
+    end
+
     sanitized_string = Parser.parse(string)
     JSON.parse(sanitized_string, **opts)
   end
